fe7f21560d5432ab0a65d56f32c278b36f48507c
SmithConnor <connorsmith010@gmail.com>
2020-02-06 10:05:28 +1100

    Update help files

--- R/vivid.R
+++ R/vivid.R
@@ -1,6 +1,4 @@
-
 #' VIVID
-#'
 #' @param x input matrix, rows represent observations and columns represent
 #' features. column names and row names are recommended.
 #' @param y response variable, restricted to two classes.
@@ -18,10 +16,32 @@
 #' @importFrom magrittr %>%
 #' @importFrom parallel detectCores
 #'
-#' @return
+#' @return A list of results:
+#' \itemize{
+#' \item{coefficients}: A numeric matrix of estimated coefficient values over re-samples.
+#' \item{varMat}: A numeric matrix of the estimated variance matrix explain in the VIVID method.
+#' \item{varClust}: The hclust() output of the clustering performed in the VIVID method.
+#' \item{selection}: A boolean matrix where each row indicates a model identified as a potential final model.
+#' \item{sizes}: A integer vector of model sizes in the seleciton path.
+#' \item{compareMethod}: A character value indicating the Information Criterion used for the results.
+#' \item{compareValues}: A numeric vector of Infomation Criterion values for each model in the selection path.
+#' \item{optModel}: A boolean vector indicating whether or not each feature is include in the final model.
+#' \item{optFeatures}: A character vector containing all features in the final model.
+#' \item{vividSplit}: A boolean value indicating whether or not this output is from the vivid_split() funciton.
+#' }
 #' @export
 #'
-#' @examples 1
+#' @examples 
+#' library('ropls')
+#' data("sacurine") #Load sacurine dataset from the 'ropls' package
+#'
+#' dat <- sacurine$dataMatrix
+#' outcomes <- sacurine$sampleMetadata$gender
+#' 
+#' vivid_results = vivid(x = dat,
+#'       y = outcomes)
+#'       
+#' vivid_results$optFeatures
 
 vivid =
   function(x,
